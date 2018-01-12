e7c4b63a721cc4efa7acf8a70350c853ab2a4d78
John W <jmweiss87@gmail.com>
2018-01-12 16:49:54 -0500

    we are creating a new file

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('hello');
+// to open command line, open integrated directory under view
+// cd change directory
+// ls list director
+// ls -a
+// ls -l
+// ls -al
+// . reps current director
+// cd .. takes you to parent directory
+// pwd print working directory
+// cd expcects a path to a directory
+// git status (checking status with of rep)
